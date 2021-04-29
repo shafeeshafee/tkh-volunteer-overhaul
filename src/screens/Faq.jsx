20ed8ee6bd738cfb2f8137df845d475629cdaa2d
Shafee <shafeelinks@gmail.com>
2021-04-29 12:54:04 -0400

    faq boilerplate changed

--- src/screens/Faq.jsx
+++ src/screens/Faq.jsx
@@ -7,59 +7,56 @@ const Faq = () => {
 				<h2 className="text-3xl font-extrabold leading-9 border-b-2 border-gray-100 text-gray-900 mb-12">FAQs</h2>
 				<ul className="flex items-start gap-8 flex-wrap">
 					<li className="w-2/5">
-						<p className="text-lg font-medium leading-6 text-gray-900">What is a home energy rating?</p>
+						<p className="text-lg font-medium leading-6 text-gray-900">What is the question?</p>
 						<p className="mt-2">
 							<p className="text-base leading-6 text-gray-500">
-								A home energy rating is an estimated calculation into a homes potential energy usage, which will determine the amount of
-								heating and cooling required to make its occupants comfortable. It produces a star rating dependant on the amount of heating
-								and cooling loads which will be required, from 0 to 10 stars.
+								Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos quasi sint provident autem beatae voluptatum optio asperiores
+								ducimus ratione, repellendus quidem doloremque distinctio sequi et mollitia delectus consequuntur, maiores nobis.
 							</p>
 						</p>
 					</li>
 					<li className="w-2/5">
-						<p className="text-lg font-medium leading-6 text-gray-900">Why do I need a 6 Star energy rating?</p>
+						<p className="text-lg font-medium leading-6 text-gray-900">What is the question?</p>
 						<p className="mt-2">
 							<p className="text-base leading-6 text-gray-500">
-								In most Australian states the government requires that all new homes and apartments (along with certain types of building
-								extensions) built since 2010 be energy rated and achieve a minimum of 6 Stars.
+								Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos quasi sint provident autem beatae voluptatum optio asperiores
+								ducimus ratione, repellendus quidem doloremque distinctio sequi et mollitia delectus consequuntur, maiores nobis.
 							</p>
 						</p>
 					</li>
 					<li className="w-2/5">
-						<p className="text-lg font-medium leading-6 text-gray-900">What is the general cost of an energy rating?</p>
+						<p className="text-lg font-medium leading-6 text-gray-900">What is the question?</p>
 						<p className="mt-2">
 							<p className="text-base leading-6 text-gray-500">
-								Simple energy rating prices vary greatly on the size and type of building, generally an energy rating will cost somewhere
-								between $130 to $700+.
+								Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos quasi sint provident autem beatae voluptatum optio asperiores
+								ducimus ratione, repellendus quidem doloremque distinctio sequi et mollitia delectus consequuntur, maiores nobis.
 							</p>
 						</p>
 					</li>
 					<li className="w-2/5">
-						<p className="text-lg font-medium leading-6 text-gray-900">
-							What information do I need to supply for an energy rating to be completed??
-						</p>
+						<p className="text-lg font-medium leading-6 text-gray-900">What is the question?</p>
 						<p className="mt-2">
 							<p className="text-base leading-6 text-gray-500">
-								The information required to complete a full and comprehensive energy report are the following final working drawings: Site
-								Plan, Floor Plan, Elevations, Sections, Lighting layout and window schedule (including sizes of the existing windows).
+								Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos quasi sint provident autem beatae voluptatum optio asperiores
+								ducimus ratione, repellendus quidem doloremque distinctio sequi et mollitia delectus consequuntur, maiores nobis.
 							</p>
 						</p>
 					</li>
 					<li className="w-2/5">
-						<p className="text-lg font-medium leading-6 text-gray-900">Does building an extension need an energy rating?</p>
+						<p className="text-lg font-medium leading-6 text-gray-900">What is the question?</p>
 						<p className="mt-2">
 							<p className="text-base leading-6 text-gray-500">
-								Depended on the size of the extension you are building there is a chance that it too will need to be energy rated. It&#x27;s
-								always best to check first before going ahead with construction.
+								Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos quasi sint provident autem beatae voluptatum optio asperiores
+								ducimus ratione, repellendus quidem doloremque distinctio sequi et mollitia delectus consequuntur, maiores nobis.
 							</p>
 						</p>
 					</li>
 					<li className="w-2/5">
-						<p className="text-lg font-medium leading-6 text-gray-900">What is the general cost of an energy rating?</p>
+						<p className="text-lg font-medium leading-6 text-gray-900">What is the question?</p>
 						<p className="mt-2">
 							<p className="text-base leading-6 text-gray-500">
-								Depended on the size of the extension you are building there is a chance that it too will need to be energy rated. It&#x27;s
-								always best to check first before going ahead with construction.
+								Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos quasi sint provident autem beatae voluptatum optio asperiores
+								ducimus ratione, repellendus quidem doloremque distinctio sequi et mollitia delectus consequuntur, maiores nobis.
 							</p>
 						</p>
 					</li>
