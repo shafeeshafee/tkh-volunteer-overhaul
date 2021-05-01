508550f25d47217fe24616540abb94de5cd64056
Shafee <shafeelinks@gmail.com>
2021-05-01 16:03:49 -0400

    filter dropdown and search on a same line now

--- src/screens/Volunteers.jsx
+++ src/screens/Volunteers.jsx
@@ -41,10 +41,10 @@ const Volunteers = () => {
 	return (
 		<div>
 			<Navbar />
-			<h1 className="text-5xl">TKH Volunteers</h1>
-			<hr />
+
+			<h1 className="text-5xl font-headings pt-24 font-bold text-tkhpurple">Our Volunteers</h1>
 			{/* dropdown */}
-			<div className="relative inline-flex pt-20">
+			<div className="relative inline-flex pt-10">
 				<svg className="w-2 h-2 absolute top-0 right-0 m-4 pointer-events-none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 412 232">
 					<path
 						d="M206 171.144L42.678 7.822c-9.763-9.763-25.592-9.763-35.355 0-9.763 9.764-9.763 25.592 0 35.355l181 181c4.88 4.882 11.279 7.323 17.677 7.323s12.796-2.441 17.678-7.322l181-181c9.763-9.764 9.763-25.592 0-35.355-9.763-9.763-25.592-9.763-35.355 0L206 171.144z"
@@ -52,37 +52,39 @@ const Volunteers = () => {
 						fill-rule="nonzero"
 					/>
 				</svg>
-				<select
-					id="skillSelect"
-					defaultValue=""
-					onChange={updateAvailableVolunteers}
-					className="border font-headings text-lg border-gray-300 rounded-full text-gray-600 h-10 pl-5 pr-10 bg-white hover:border-gray-400 focus:outline-none appearance-none"
-				>
-					<option value="" selected="selected" disabled hidden>
-						Select skills of interest
-					</option>
-					<option value="javascript">JavaScript</option>
-					<option value="c++">C++</option>
-					<option value="python">Python</option>
-					<option value="sql">SQL</option>
-					<option value="lua">Lua</option>
-					<option value="technical interviews">Technical Interview Prep</option>
-					<option value="behaviorial interviews">Behavioral Interview Prep</option>
-				</select>
-			</div>
-			<br />
-			{/* Search component goes below */}
-			<div className="flex justify-center items-center font-headings">
-				<p className="mx-3">Search:</p>
-				<input
-					className="border shadow-lg h-10 w-56 text-center my-10"
-					id="volunterSearchCriteria"
-					onChange={updateAvailableVolunteers}
-					type="search"
-					placeholder="Search e.g., JavaScript"
-				/>
-			</div>
+				<div className="flex justify-center items-center flex-wrap">
+					<select
+						id="skillSelect"
+						defaultValue=""
+						onChange={updateAvailableVolunteers}
+						className="border font-headings text-lg border-gray-300 rounded-full text-gray-600 h-10 pl-5 pr-10 bg-white hover:border-gray-400 focus:outline-none appearance-none"
+					>
+						<option value="" selected="selected" disabled hidden>
+							Select skills of interest
+						</option>
+						<option value="JavaScript">JavaScript</option>
+						<option value="c++">C++</option>
+						<option value="python">Python</option>
+						<option value="sql">SQL</option>
+						<option value="lua">Lua</option>
+						<option value="technical interviews">Technical Interview Prep</option>
+						<option value="behaviorial interviews">Behavioral Interview Prep</option>
+					</select>
 
+					<br />
+					{/* Search component goes below */}
+					<div className="flex justify-center items-center font-headings">
+						<p className="mx-0 lg:ml-12 lg:mr-2">Search:</p>
+						<input
+							className="border shadow-lg h-10 w-56 text-center my-10"
+							id="volunterSearchCriteria"
+							onChange={updateAvailableVolunteers}
+							type="search"
+							placeholder="Search e.g., JavaScript"
+						/>
+					</div>
+				</div>
+			</div>
 			{/* List of volunteers below */}
 			<ul className="flex justify-center flex-wrap" id="relevantVoluteersLst">
 				{volunteers.technical.map((vol) => {
